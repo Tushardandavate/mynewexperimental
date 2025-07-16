f02b246904725b9986ac2858b207551f995d1765
Tushar Dandavate <tushardandavate2004@gmail.com>
2025-07-16 20:15:11 +0530

    hey

--- rushi.java
+++ rushi.java
@@ -1,10 +1,12 @@
 public class rushi {
-    public static void main(String [] arg){
+    
+    public static void main(String[] arg) {
         System.out.println("hello how are you");
         System.out.println("fine......");
-        void mass(){
-            System.out.println("this is mass method.....");
-        }
-        
+        mass();  // call the method
+    }
+
+    public static void mass() {
+        System.out.println("this is mass method.....");
     }
 }
